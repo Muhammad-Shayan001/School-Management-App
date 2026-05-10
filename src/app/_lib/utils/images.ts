0a0d65de4d32dadf305ab903b1d2fdfe17bf3019
Muhammad Shayan <shayan.javed091@gmail.com>
2026-05-10 23:04:34 +0500

    new Update

---
+++ src/app/_lib/utils/images.ts
@@ -0,0 +1,8 @@
+// --- Professional Education Images (Paths) ---
+export const IMAGES = {
+  HERO: '/images/dashboard.png',
+  STUDENTS: '/images/classroom.png',
+  TEACHER: '/images/teacher.png',
+  SCHOOL_MGMT: '/images/dashboard.png',
+  CLASSROOM: '/images/classroom.png'
+};
